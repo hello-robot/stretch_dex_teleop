953ba3bda1f59e74a9d4b04cb612af80b3d7f2d1
hello-lamsey <lamsey@hello-robot.com>
2024-08-01 14:04:04 -0400

    checkin nit

--- webcam_teleop_interface.py
+++ webcam_teleop_interface.py
@@ -161,7 +161,7 @@ class WebcamArucoDetector:
                 ratio = np.clip(ratio, a_min=-1, a_max=1)
 
                 variable_angle = math.asin(ratio)
-                
+
                 if self.debug_side_marker: 
                     print('tongs_pin_joint_to_marker_cecnter = {:.2f} cm'.format(self.tongs_pin_joint_to_marker_center * 100.0))
                     print('distance_between_markers = {:.2f} cm'.format(distance_between_markers * 100.0))
@@ -413,10 +413,10 @@ class WebcamArucoDetector:
             h2 = hypotenuse * hypotenuse
             o2 = opposite_side * opposite_side
 
-            if o2 >= h2:
-                o2 = h2
+            difference_pythagorean = h2 - o2
+            difference_pythagorean = np.clip(difference_pythagorean, 0.01, float('inf'))
 
-            adjacent_side = math.sqrt(h2 - o2)
+            adjacent_side = math.sqrt(difference_pythagorean)
             grip_pos = grip_pos + (-(adjacent_side) * grip_y_axis)
             
                 
